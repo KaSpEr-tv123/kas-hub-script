9ad4bff5a51bebc7f62857cab629f4fee17dee87
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2023-10-14 18:43:44 +0300

    Add files via upload

--- script.lua
+++ script.lua
@@ -211,7 +211,7 @@ tool.Name = "Equip to Click TP"
 tool.Activated:connect(function()
 local pos = mouse.Hit+Vector3.new(0,2.5,0)
 pos = CFrame.new(pos.X,pos.Y,pos.Z)
-if mode_tp then
+if mode_tp == true then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
 end
 end)
