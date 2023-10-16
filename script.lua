10260e919a18de316349be20ab90a2c9c38d5f1e
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2023-10-16 11:52:41 +0300

    Bug fix

--- script.lua
+++ script.lua
@@ -112,7 +112,7 @@ tp.newButton("Save your position", "", function()
   positon = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 end)
 tp.newButton("TP in saved position", "", function()
-  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(position.x, position.y, position.z)
 end)
 
 if game.GameId == 1268927906 then
