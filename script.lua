57ac5631e8780e45705d788438055ec8f44c1155
KaSpEr-tv123 <dimakukic1@gmail.com>
2024-03-27 19:07:28 +0200

    хз

--- script.lua
+++ script.lua
@@ -234,7 +234,7 @@ if game.GameId == 2020908522 then
     -- kill, not tp
     for i, v in pairs(game.Players:GetChildren()) do
       if v ~= game.Players.LocalPlayer then
-        if not v.Head:FindFirstChild("SeekerTitle") then
+        if not v.FindFirstChild("Head"):FindFirstChild("SeekerTitle") then
           v.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
         end
       end
