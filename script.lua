fe44a0433f50ee8320e486f07316b37ceb9f7044
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2024-04-05 21:06:36 +0000

    fix

--- script.lua
+++ script.lua
@@ -404,43 +404,106 @@ blobs.newButton("Auto farm blobs", "", function()
         spawn(autoFarmBlobs) -- Запускаем функцию в новом потоке
     end
 end)
-local Radius = 400 -- Радиус для активации ауры убийства
-local DamageAmount = 15 -- Количество урона, которое вы наносите
+local connections = getgenv().configs and getgenv().configs.connection
+if connections then
+    local Disable = configs.Disable
+    for i,v in connections do
+        v:Disconnect() 
+    end
+    Disable:Fire()
+    Disable:Destroy()
+    table.clear(configs)
+end
 
-local Exploiter = game.Players.LocalPlayer
-local Enabled = false
+local Disable = Instance.new("BindableEvent")
+getgenv().configs = {
+    connections = {},
+    Disable = Disable,
+    Size = Vector3.new(10,10,10),
+    DeathCheck = true
+}
 
-function KillAura()
-    while Enabled do
-        for _, Player in pairs(game.Players:GetPlayers()) do
-          if Player ~= Exploiter then
-            local Character = Player.Character
-            if Character and Character:FindFirstChild("HumanoidRootPart") and Character:FindFirstChild("Humanoid") then
-                local Entity = Character.HumanoidRootPart
-                if Entity and Character.Humanoid.Health > 0 then
-                    local ExploiterPosition = Exploiter.Character and Exploiter.Character:FindFirstChild("HumanoidRootPart") and Exploiter.Character.HumanoidRootPart.Position
-                    local PlayerPosition = Entity.Position
+local Players = cloneref(game:GetService("Players"))
+local RunService = cloneref(game:GetService("RunService"))
+local lp = Players.LocalPlayer
+local Run = true
+local Ignorelist = OverlapParams.new()
+Ignorelist.FilterType = Enum.RaycastFilterType.Include
 
-                    if ExploiterPosition then
-                        if (PlayerPosition - ExploiterPosition).magnitude < Radius then
-                            -- Нанесение урона игроку
-                            Character.Humanoid:TakeDamage(DamageAmount)
-                            print("Нанесен урон игроку:", Player.Name)
-                        end
-                    end
-                end
-            end
-          end
-        end
-        wait(0.1) -- Добавляем небольшую задержку между проверками
+local function getchar(plr)
+    local plr = plr or lp
+    return plr.Character
+end
+
+local function gethumanoid(plr)
+    local char = plr:IsA("Model") and plr or getchar(plr)
+
+    if char then
+        return char:FindFirstChildWhichIsA("Humanoid")
     end
 end
 
-blobs.newButton("Kill Aura", "", function()
-    Enabled = not Enabled
-    if Enabled then
-        spawn(KillAura) -- Запускаем функцию в новом потоке
+local function IsAlive(Humanoid)
+    return Humanoid and Humanoid.Health > 0
+end
+
+local function GetTouchInterest(Tool)
+    return Tool and Tool:FindFirstChildWhichIsA("TouchTransmitter",true)
+end
+
+local function GetCharacters(LocalPlayerChar)
+    local Characters = {}
+    for i,v in Players:GetPlayers() do
+        table.insert(Characters,getchar(v))
     end
+    table.remove(Characters,table.find(Characters,LocalPlayerChar))
+    return Characters
+end
+
+local function Attack(Tool,TouchPart,ToTouch)
+    if Tool:IsDescendantOf(workspace) then
+        Tool:Activate()
+        firetouchinterest(TouchPart,ToTouch,1)
+        firetouchinterest(TouchPart,ToTouch,0)
+    end
+end
+
+table.insert(getgenv().configs.connections,Disable.Event:Connect(function()
+    Run = false
+end))
+
+while Run do
+    local char = getchar()
+    if IsAlive(gethumanoid(char)) then
+        local Tool = char and char:FindFirstChildWhichIsA("Tool")
+        local TouchInterest = Tool and GetTouchInterest(Tool)
+
+        if TouchInterest then
+            local TouchPart = TouchInterest.Parent
+            local Characters = GetCharacters(char)
+            Ignorelist.FilterDescendantsInstances = Characters
+            local InstancesInBox = workspace:GetPartBoundsInBox(TouchPart.CFrame,TouchPart.Size + getgenv().configs.Size,Ignorelist)
+
+            for i,v in InstancesInBox do
+                local Character = v:FindFirstAncestorWhichIsA("Model")
+
+                if table.find(Characters,Character) then
+                    if getgenv().configs.DeathCheck then                    
+                        if IsAlive(gethumanoid(Character)) then
+                            Attack(Tool,TouchPart,v)
+                        end
+                    else
+                        Attack(Tool,TouchPart,v)
+                    end
+                end
+            end
+        end
+    end
+    RunService.Heartbeat:Wait()
+end
+
+blobs.newButton("Kill Aura", "", function()
+    Run = not Run
 end)
 
 
