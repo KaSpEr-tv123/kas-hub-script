391e8d8d2935ce57076f258e569efe7655b24e3e
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2024-04-07 08:38:04 +0300

    Fix

--- script.lua
+++ script.lua
@@ -390,7 +390,7 @@ function autoFarmBlobs()
                     end
                 end
 
-                if teleportAllowed then
+                if teleportAllowed and game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame ~= nearestCoin.CFrame then
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = nearestCoin.CFrame
                 end
             else
