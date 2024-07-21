99e788d7b04eb9c757d91593882bf6f1368cecdf
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2024-07-21 15:29:53 +0300

    update kasperesp.luan

--- kasperesp.lua
+++ kasperesp.lua
@@ -1,8 +1,7 @@
-_G.FriendColor = Color3.fromRGB(0, 0, 255)
-_G.EnemyColor = Color3.fromRGB(255, 0, 0)
+_G.FriendColor = Color3.fromRGB(128, 0, 128)
+_G.EnemyColor = Color3.fromRGB(255, 255, 0)
 _G.UseTeamColor = true
 
---------------------------------------------------------------------
 local Holder = Instance.new("Folder", game.CoreGui)
 Holder.Name = "ESP"
 
