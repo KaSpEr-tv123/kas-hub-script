54c6b4a417723a4ecd552595013a7c91b6be6225
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2024-07-21 14:44:46 +0300

    update kasperfly.lua

--- kasperfly.lua
+++ kasperfly.lua
@@ -15,8 +15,8 @@ main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 main.ResetOnSpawn = false
 
 Frame.Parent = main
-Frame.BackgroundColor3 = Color3.fromRGB(163, 255, 137)
-Frame.BorderColor3 = Color3.fromRGB(103, 221, 213)
+Frame.BackgroundColor3 = Color3.fromRGB(138, 43, 226)
+Frame.BorderColor3 = Color3.fromRGB(123, 104, 238)
 Frame.Position = UDim2.new(0.100320168, 0, 0.379746825, 0)
 Frame.Size = UDim2.new(0, 190, 0, 57)
 
