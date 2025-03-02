fa89187c45e46fa1fcba90ae0d7c396dc06dfdb2
KaSpEr-tv123 <dimakukic1@gmail.com>
2025-03-02 21:00:22 +0200

    fix ps99

--- script.lua
+++ script.lua
@@ -644,9 +644,9 @@ if game.GameId == 4777817887 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BladeBall/main/redz9999"))()
 end
 
-local brekablesFolder = game.Workspace:WaitForChild("_THINGS"):WaitForChild("Brekables")
-
 local function findAndClickClosestCoin()
+    local brekablesFolder = game.Workspace:WaitForChild("__THINGS"):WaitForChild("Breaskables")
+
     local player = game.Players.LocalPlayer
     local character = player.Character or player.CharacterAdded:Wait()
     local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
