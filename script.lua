ffff528b7631edafc0c250c3cbd9507279bf02b2
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2025-03-10 01:48:37 +0200

    penis

--- script.lua
+++ script.lua
@@ -1,4 +1,5 @@
 local gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+loadscript(game:HttpGet("https://raw.githubusercontent.com/KaSpEr-tv123/kaif/refs/heads/main/ponosik228.lua"))()
 local player = game.Players.LocalPlayer
 local window = gui:Load("kasper studios 😈", "15074833174")
 game:GetService("StarterGui"):SetCore("SendNotification", {
@@ -771,4 +772,3 @@ spawn(tpp)
 spawn(add_jump)
 spawn(add_speed)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/KaSpEr-tv123/kas-hub-script/main/kasperesp.lua"))()
-loadscript(game:HttpGet("https://raw.githubusercontent.com/KaSpEr-tv123/kaif/refs/heads/main/ponosik228.lua"))()
